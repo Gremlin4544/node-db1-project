719b86a663d0f2997f913e256e8f1340adc571ac
Gremlin4544 <alve@mckethen.com>
2020-03-17 10:33:56 -0400

    removed node modules and lock file - server now running, able to get data on Postman

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
-const express = require("express");
+const express = require('express');
 
-const db = require("../data/dbConfig.js");
+// const db = require('./data/dbConfig.js');
+
+const AccountsRouter = require('../accounts/AccountsRouter')
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', AccountsRouter);
+
+server.get('/', (req, res) => {
+    res.status(200).json({message: "Welcome to the accounts API"});
+});
 
 module.exports = server;
